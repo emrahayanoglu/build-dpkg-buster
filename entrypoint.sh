55f3dddabb91f9f01441fa688d9c53679990e76a
Emrah Ayanoglu <emrah.ayanoglu@schindler.com>
2023-03-13 12:05:13 +0300

    introduced multi-arch approach

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 set -e
 # Set the install command to be used by mk-build-deps (use --yes for non-interactive)
-install_tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes"
+install_tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes  ${INPUT_architecture}"
 # Install build dependencies automatically
 mk-build-deps --install --tool="${install_tool}" debian/control
 # Build the package
-dpkg-buildpackage $@
+dpkg-buildpackage $@ --host-arch ${INPUT_architecture}
 # Output the filename
 cd ..
 filename=`ls *.deb | grep -v -- -dbgsym`
