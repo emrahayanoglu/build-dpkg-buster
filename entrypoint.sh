007554c0aab95bb49443d4ae8f941b55f685b2c8
Emrah Ayanoglu <emrah.ayanoglu@schindler.com>
2023-03-13 12:45:34 +0300

    entrypoint fixed with right architecture param

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,7 @@ dpkg --add-architecture $1
 apt-get update
 apt-get install -y crossbuild-essential-$1
 
-# Set the install command to be used by mk-build-deps (use --yes for non-interactive)
-install_tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes  $1"
-# Install build dependencies automatically
-mk-build-deps --install --tool="${install_tool}" debian/control
+apt-get -o Debug::pkgProblemResolver=yes -y --force-yes build-dep -a$1 .
 # Build the package
 dpkg-buildpackage $@ --host-arch $1
 # Output the filename
