a3c7d7e71707ccf5eba5e0da57e5190c4f49607e
Emrah Ayanoglu <emrah.ayanoglu@schindler.com>
2023-03-13 12:48:58 +0300

    entrypoint fixed with right architecture param

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ apt-get install -y crossbuild-essential-$1
 
 apt-get -o Debug::pkgProblemResolver=yes -y --force-yes build-dep -a$1 .
 # Build the package
-dpkg-buildpackage $@ --host-arch $1
+dpkg-buildpackage $@ -a$1
 # Output the filename
 cd ..
 filename=`ls *.deb | grep -v -- -dbgsym`
