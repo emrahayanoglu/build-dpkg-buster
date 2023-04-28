d93a1e21d3c08f3bcb8d266f6de6da1202379435
Emrah Ayanoglu <emrah.ayanoglu@schindler.com>
2023-04-28 15:49:34 +0300

    multirepo build introduced

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,23 @@ dpkg --add-architecture $1
 apt-get update
 apt-get install -y crossbuild-essential-$1
 
+cd paho.mqtt.c
 apt-get -o Debug::pkgProblemResolver=yes -y --force-yes build-dep -a$1 .
 # Build the package
 echo "Package building is started!"
 dpkg-buildpackage -b $2 --host-arch $1 || true
 echo "Package is built!"
+
+cd ..
+dpkg -i libpaho*dev*.deb
+
+cd main
+apt-get -o Debug::pkgProblemResolver=yes -y --force-yes build-dep -a$1 .
+# Build the package
+echo "Package building is started!"
+dpkg-buildpackage -b $2 --host-arch $1 || true
+echo "Package is built!"
+
 # Output the filename
 cd ..
 ls -l *.deb
